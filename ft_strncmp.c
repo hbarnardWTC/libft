10c32f4dfbe20272819a6b39e8a1cdbca5a03a77
Heinrich Barnard <hbarnard@c3r8s8.wethinkcode.co.za>
2019-06-26 10:20:32 +0200

    Quick fix

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,14 +14,18 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int i;
+	size_t			i;
+	unsigned char	*c1;
+	unsigned char	*c2;
 
 	i = 0;
-	while ((s1[i] || s2[i]) && i < (int)n)
+	c1 = (unsigned char *)s1;
+	c2 = (unsigned char *)s2;
+	while ((s1[i] || s2[i]) && i < n)
 	{
-		if ((unsigned char)s1[i] > (unsigned char)s2[i])
+		if (c1[i] > c2[i])
 			return (1);
-		else if ((unsigned char)s1[i] < (unsigned char)s2[i])
+		else if (c1[i] < c2[i])
 			return (-1);
 		i++;
 	}
